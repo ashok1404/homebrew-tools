4b220b8aca08be9e8b210d43b4ac54b54e6dbbdc
jp-aloha <jaib@alohatechnology.com>
2026-06-04 11:54:56 +0530

    Remove old binaries

--- Formula/bttinstrumentor.rb
+++ Formula/bttinstrumentor.rb
@@ -3,17 +3,11 @@ class Bttinstrumentor < Formula
   homepage "https://www.bluetriangle.com"
   version "1.0.0"
 
-  if Hardware::CPU.arm?
-    url "https://github.com/ashok1404/BTTInstrumentor/releases/download/1.0.0/BTTInstrumentor-arm64"
-    sha256 "2be2b14a7488174e4605de492e86b172f760ecd7075c55b755766a726e591cba"
-  else
-    url "https://github.com/ashok1404/BTTInstrumentor/releases/download/1.0.0/BTTInstrumentor-x86_64"
-    sha256 "2be2b14a7488174e4605de492e86b172f760ecd7075c55b755766a726e591cba"
-  end
+  url "https://github.com/ashok1404/BTTInstrumentor/releases/download/1.0.0/BTTInstrumentor"
+  sha256 "PASTE_SHA256_HERE"
 
   def install
-    binary = Hardware::CPU.arm? ? "BTTInstrumentor-arm64" : "BTTInstrumentor-x86_64"
-    bin.install binary => "BTTInstrumentor"
+    bin.install "BTTInstrumentor"
   end
 
   test do
