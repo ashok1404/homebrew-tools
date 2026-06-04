5f2fb115b059a10fb2f6ff4617d9d9df92fa49f1
jp-aloha <jaib@alohatechnology.com>
2026-06-04 12:17:59 +0530

    Update formula

--- Formula/bttinstrumentor.rb
+++ Formula/bttinstrumentor.rb
@@ -4,7 +4,7 @@ class Bttinstrumentor < Formula
   version "1.0.0"
 
   url "https://github.com/ashok1404/BTTInstrumentor/releases/download/1.0.0/BTTInstrumentor"
-  sha256 "PASTE_SHA256_HERE"
+  sha256 "3ae9b0c19a60100880f826ce846b0da67bf49bb10dae371b178e33eec437c5c9"
 
   def install
     bin.install "BTTInstrumentor"
