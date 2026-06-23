10d0c153e602ba8d1a73190ddc6c7ac59efa3a32
radhika1290singh-design <radhika1290.singh@gmail.com>
2026-06-24 02:51:48 +0530

    update sha

--- Formula/bttinstrumentor.rb
+++ Formula/bttinstrumentor.rb
@@ -1,10 +1,10 @@
 class Bttinstrumentor < Formula
   desc "BlueTriangle SwiftUI automatic screen tracking instrumentor"
-  homepage "https://www.bluetriangle.com"
+  homepage "https://www.ashok1404.com"
   version "1.0.0"
 
   url "https://github.com/ashok1404/BTTInstrumentor/releases/download/1.0.0/BTTInstrumentor"
-  sha256 "7002a8b185fa1b7eabd3089226fbb37013104414ca751ea465b425d83be21ae6"
+  sha256 "20183afecd39121994374a1d40294433793840d6e59c4c2d26d37ead5ba73db4"
 
   def install
     bin.install "BTTInstrumentor"
