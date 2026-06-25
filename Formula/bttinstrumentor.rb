1eacfec52734e9a944871f949bb9a216551074f8
ashok1404 <ashokmca.iet@gmail.com>
2026-06-25 14:57:15 +0530

    Update bttinstrumentor.rb

--- Formula/bttinstrumentor.rb
+++ Formula/bttinstrumentor.rb
@@ -4,7 +4,7 @@ class Bttinstrumentor < Formula
   version "1.0.0"
 
   url "https://github.com/ashok1404/BTTInstrumentor/releases/download/1.0.0/BTTInstrumentor"
-  sha256 "490249b93b7c35a21e714c321bcf61373a346ec70df411fd4f883ffdacd9e0de"
+  sha256 "30a0345361b6d62879e848a52ac4e314b9e3c35ddbe11b13664dfd7caf45f2bf"
 
   def install
     bin.install "BTTInstrumentor"
